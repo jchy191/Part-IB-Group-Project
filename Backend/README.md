d62053db8c74dd4944057c5469c1e919947bd357
Joshua Chin <joshuachin191@gmail.com>
2023-02-09 12:03:08 +0000

    Added connection to Google Cloud SQL

--- Backend/README.md
+++ Backend/README.md
@@ -15,12 +15,17 @@ A few prerequisite software is required in order to run and develop locally.
 2. Add the `.env` file into the `Part-IB-Group-Project/Backend` (i.e., create a `Part-IB-Group-Project/Backend/.env` file)\
 This step is required so that we do not commit & push any API keys on git.
 3. Run `python3 -m venv env` to create & enter the python virtual environment, and then `pip install -r requirements.txt`
+4. To be able to access the Google Cloud SQL database, install Cloud SQL proxy (ask Joshua)
 
 ## Development in Python venv (Virtual Environment)
-1. In the `Part-IB-Group-Project/Backend` directory, run\
+1. Start Cloud SQL Auth proxy\
+   Mac/Linux: `./cloud_sql_proxy "clj-377111:europe-west2:clj-maps" -p 5432`\
+   Windows: `cloud_sql_proxy.exe "clj-377111:europe-west2:clj-maps" -p 5432`
+2. In the `Part-IB-Group-Project/Backend` directory, run\
    Mac/Linux: `source env/bin/activate`\
    Windows: `use env/bin/activate`
-2. In the virtual environment, control the server using the `python manage.py ...` command, e.g., `python manage.py runserver`.
+3. In the virtual environment, run `export GOOGLE_CLOUD_PROJECT=clj-377111` and `export USE_CLOUD_SQL_AUTH_PROXY=true`
+4. In the virtual environment, control the server using the `python manage.py ...` command, e.g., `python manage.py runserver`.
 
 ## Development in Docker
 1. Ensure Docker Desktop is running.
--- Backend/server/settings.py
+++ Backend/server/settings.py
@@ -1,15 +1,65 @@
+import io
+import os
+from urllib.parse import urlparse
+
+import environ
+import google.auth
+from google.cloud import secretmanager
 from pathlib import Path
 from dotenv import load_dotenv
-import os
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = int(os.environ.get("DEBUG", default=0))
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+# [START cloudrun_django_secret_config]
+# SECURITY WARNING: don't run with debug turned on in production!
+# Change this to "False" when you are ready for production
+env = environ.Env(DEBUG=(bool, True))
+env_file = os.path.join(BASE_DIR, ".env")
+
+# Attempt to load the Project ID into the environment, safely failing on error.
+try:
+    _, os.environ["GOOGLE_CLOUD_PROJECT"] = google.auth.default()
+# except google.auth.exceptions.DefaultCredentialsError:
+except Exception:
+    pass
+
+if os.path.isfile(env_file):
+    # Use a local secret file, if provided
+    env.read_env(env_file)
+elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
+    # Pull secrets from Secret Manager
+    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+
+    client = secretmanager.SecretManagerServiceClient()
+    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
+    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+
+    env.read_env(io.StringIO(payload))
+else:
+    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
+# [END cloudrun_django_secret_config]
+
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG")
+
+# [START cloudrun_django_csrf]
+# SECURITY WARNING: It's recommended that you use this when
+# running in production. The URL will be known once you first deploy
+# to Cloud Run. This code takes the URL and converts it to both these settings formats.
+CLOUDRUN_SERVICE_URL = env("CLOUDRUN_SERVICE_URL", default=None)
+if CLOUDRUN_SERVICE_URL:
+    ALLOWED_HOSTS = [urlparse(CLOUDRUN_SERVICE_URL).netloc]
+    CSRF_TRUSTED_ORIGINS = [CLOUDRUN_SERVICE_URL]
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+else:
+    ALLOWED_HOSTS = ["*"]
+# [END cloudrun_django_csrf]
 
 
 # Application definition
@@ -60,13 +110,16 @@ WSGI_APPLICATION = "server.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# [START cloudrun_django_database_config]
+# Use django-environ to parse the connection string
+DATABASES = {"default": env.db()}
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# If the flag as been set, configure to use proxy
+if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
+    DATABASES["default"]["HOST"] = "127.0.0.1"
+    DATABASES["default"]["PORT"] = 5432
+
+# [END cloudrun_django_database_config]
 
 
 # Password validation
