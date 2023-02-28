d3853b9a9e4cb2591eab34c96aacc75aea4af397
sowalks <98213865+sowalks@users.noreply.github.com>
2023-02-28 16:37:52 +0000

    Comment Section and Drawer
    Added CommentSection and Filter Drawer components.

---
+++ Frontend/src/Components/Drawer/Drawer.jsx
@@ -0,0 +1,123 @@
+import * as React from 'react';
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import accessCategories from "../AccessCategories/AccessCategories";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CommentSection from "../CommentSection/CommentSection";
+
+const drawerWidth = 240;
+
+function ResponsiveDrawer(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <Typography variant="h6" sx={{ p: 2 }}>
+        Filters
+      </Typography>
+      <FormGroup sx={{ ml: 2 }}>
+        {Object.entries(accessCategories).map(([key, cat], index) => (
+          <FormControlLabel
+            control={
+              <Checkbox
+                defaultChecked
+                sx={{
+                  color: cat.colour,
+                  "&.Mui-checked": { color: cat.colour }
+                }}
+              />
+            }
+            label={cat.name}
+            labelPlacement="end"
+          />
+        ))}
+      </FormGroup>
+      <Typography variant="h6" sx={{ p: 2 }}>
+        About
+      </Typography>
+    </div>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Cambridge Land Justice
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      >
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+      >
+        <Toolbar />
+        <CommentSection />
+      </Box>
+    </Box>
+  );
+}
+
+
+export default ResponsiveDrawer;
