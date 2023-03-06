99a1a6089434150267a01586dd10136b7a989e77
mss-13 <105442689+mss-13@users.noreply.github.com>
2023-03-06 14:01:06 +0000

    Fixed admin display

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -6,8 +6,7 @@ from .models import Entry
 class EntryAdmin(admin.ModelAdmin):
     admin.site.site_header = 'Just Maps Admin Page'
     admin.site.site_title = 'Just Maps Admin'
-    list_display = ('id', 'created', 'pid', 'lng', 'lat',
-                    'open', 'comment', 'reported', 'pinned')
+    list_display = ('id', 'name', 'created', 'pid', 'open', 'friendly', 'quiet', 'groups', 'spend', 'comment', 'reported', 'pinned', 'address')
     search_fields = ['id', 'created', 'pid', 'lng',
                      'lat', 'open', 'comment', 'reported', 'pinned']
     list_filter = ('reported', 'pinned', 'pid')
