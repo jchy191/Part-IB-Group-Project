92825e49b4b92ccb071e535157863c1df5409f18
Luca Choteborsky <luca.choteborsky@outlook.com>
2023-03-08 15:16:44 +0000

    Fixed the Admin Delete
    The Admin page does not call the delete view, so I had to overwrite the default view to call the required methods

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Entry
+from .views import updateacc, updatetype
 
 
 @admin.register(Entry)
@@ -10,3 +11,12 @@ class EntryAdmin(admin.ModelAdmin):
     search_fields = ['id', 'created', 'pid', 'lng',
                      'lat', 'open', 'comment', 'reported', 'pinned']
     list_filter = ('reported', 'pinned', 'name')
+
+    def delete_queryset(self, request, queryset):
+        
+        for entry_values in queryset.values():
+            updateacc(entry_values, -1)
+            updatetype(entry_values)
+
+        queryset.delete()
+
